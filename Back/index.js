1b027eb7f5ed1197667115e9d747a0c5cf864e08
HanseKim <kimbird2002@naver.com>
2025-05-12 19:35:05 +0900

    5/12

--- Back/index.js
+++ Back/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const cors = require("cors");
-const app = express(); // ← 이 줄이 없어!
+const app = express();
 
 
 // app.use(cors()); // ✅ CORS 허용
--- Back/utils/io.js
+++ Back/utils/io.js
@@ -1,34 +1,6 @@
 const userController = require("../Controllers/user.controllers");
 const User = require("../Models/user");
 
-// module.exports = function (io) {
-//     io.on("connection", async (socket) => {
-//         console.log("connected", socket.id);
-
-//         socket.on("login", async (user, partner, cb) => {
-//             console.log("user", user);
-//             try {
-//                 const userdata = await userController.saveUser(user, socket.id);
-//                 socket.join(user); // ✅ 자기 이름으로 방 참여
-//                 cb({ ok: true, data: userdata });
-//             } catch (error) {
-//                 cb({ ok: false, error: error.message });
-//             }
-//         });
-        
-
-//         socket.on("chat_message", (data) => {
-//             const { sender, receiver, message } = data;
-//             console.log(`From ${sender} to ${receiver}: ${message}`);
-        
-//             io.to(receiver).emit("chat_message", { sender, message }); // 방으로 전송
-//         });
-
-//         socket.on("disconnect", () => {
-//             console.log("disconnect");
-//         });
-//     });
-// };
 module.exports = function (io) {
     io.on("connection", async (socket) => {
         console.log("connected", socket.id);
